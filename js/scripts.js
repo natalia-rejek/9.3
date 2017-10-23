212985026bdf216ca4619a0407f5077a2453771a
Natalia <nettel@wp.pl>
2017-10-23 13:35:32 +0200

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "Triceratops is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+
+var nameUpperCased = dinosaur.toUpperCase();
+console.log(nameUpperCased);
+
+var partOfSentence = nameUpperCased.slice(0,11);
+
+var replacedText = text.replace('Velociraptor', partOfSentence);
+
+var mottoCharsAfter = replacedText.substr(0, replacedText.length/2);
+
+console.log(mottoCharsAfter)
