1011dc77afba0efd0b5f79f0e41d7bb1a200754b
Natalia <nettel@wp.pl>
2017-10-24 13:56:02 +0200

    removed comment from js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// scripts.js
-
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 var dinosaur = "Triceratops is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 
